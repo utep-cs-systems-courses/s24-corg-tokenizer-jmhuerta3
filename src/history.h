7e86d50d3acf58b6fb7605272eaa11d4ef4a94e6
Jazmin Mabel Huerta Huerta <jazminmabelhuertahuerta@Jazmins-MacBook-Air-7.local>
2024-03-26 15:24:13 -0600

    start of history code

--- src/history.h
+++ src/history.h
@@ -9,16 +9,44 @@ typedef struct s_Item {
 
 typedef struct s_List {
   struct s_Item *root;
+  int *size;
 } List;
 
 /* Initialize the linked list to keep the history. */
-List* init_history();
+List* init_history(){
+  
+  List* history = (List*)malloc(sizeof(List));
+
+  history->root=NULL;
+  history->size=0;
+  return history;
+} ;
 
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str);
+void add_history(List *list, char *str){
+  s_Item*  new_Item = malloc(sizeof(s_Item));
+  
+  new_Item->str = str;
+  new_Item->next=NULL;
+
+  s_Item* curr = malloc(sizeof(s_Item));
+  curr = *list->root;
+  
+  
+  if (list->root=NULL){
+    new_Item->id=0;
+    list->root = new_Item;
+    list->size =1;
+  }
+  else{
+    for (i=0;i<list->size;i++){
+      if(list->id
+    }
+  }
+};
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
