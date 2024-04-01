d182d1d0a0e9fa6fa0dc530f47bf1b158206a3f1
Jazmin Mabel Huerta Huerta <jazminmabelhuertahuerta@Jazmins-MacBook-Air-7.local>
2024-03-31 22:17:02 -0600

    deleted code from the history h file

--- src/history.h
+++ src/history.h
@@ -13,40 +13,13 @@ typedef struct s_List {
 } List;
 
 /* Initialize the linked list to keep the history. */
-List* init_history(){
-  
-  List* history = (List*)malloc(sizeof(List));
-
-  history->root=NULL;
-  history->size=0;
-  return history;
-} ;
+List* init_history() ;
 
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str){
-  s_Item*  new_Item = malloc(sizeof(s_Item));
-  
-  new_Item->str = str;
-  new_Item->next=NULL;
-
-  s_Item* curr = malloc(sizeof(s_Item));
-  curr = *list->root;
-  
-  
-  if (list->root=NULL){
-    new_Item->id=0;
-    list->root = new_Item;
-    list->size =1;
-  }
-  else{
-    for (i=0;i<list->size;i++){
-      if(list->id
-    }
-  }
-};
+void add_history(List *list, char *str);
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
