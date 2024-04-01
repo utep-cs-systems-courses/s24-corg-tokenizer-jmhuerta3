1ae6d94978f9f8453d849391b5c314b912bcc91a
Jazmin Mabel Huerta Huerta <jazminmabelhuertahuerta@Jazmins-MacBook-Air-7.local>
2024-03-31 21:38:04 -0600

    first attempt of ui code

---
+++ src/uimain.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "tokenizer.h"
+
+int main(){
+  puts("Welcome to your tokenizer:)");
+
+  fputs("Enter the phrase you want to tokenize:\n,>>", stdout);
+  fflush(stdout);
+  char str;
+
+  printf(str);
+
+  return 0;
+}
