92743e97d3390ecf09829d869ccce23d7fb25787
Jazmin Mabel Huerta Huerta <jazminmabelhuertahuerta@Jazmins-MacBook-Air-7.local>
2024-04-10 12:08:51 -0600

    testing tokenizer in uimain but have errors

--- src/uimain.c
+++ src/uimain.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "tokenizer.h"
+#include "history.h"
+#include <stdlib.h>
 
 int main(){
   puts("Welcome to your tokenizer:)");
@@ -8,7 +10,9 @@ int main(){
   fflush(stdout);
 
   char str[100];//space for my string 
-  fgets(str, sizeof(str), stdin);//read input 
+  fgets(str, sizeof(str), stdin);//read input
+  char **tokens = tokenize(str);
+  print_tokens(tokens);
 
   printf("%s \n", str);
 
