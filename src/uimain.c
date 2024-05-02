22b31048e4d2d57d164ad92e06f05dbb57621821
Jazmin Mabel Huerta Huerta <jazminmabelhuertahuerta@UTEP-83823J3.utep.edu>
2024-05-01 21:06:22 -0600

    writting the final uimain

--- src/uimain.c
+++ src/uimain.c
@@ -5,7 +5,47 @@
 
 int main(){
   puts("Welcome to your tokenizer:)");
+  bool still =true;
+  
+  while(still){
+    puts("What do you want to do?");
+    puts("1. Tokenize!");
+    puts("2. See history");
+    puts("3. Exit program");
 
+    fputs("Please enter one of the options (1-3): >>" stdout);
+    fflush(stdout);
+
+    char opt[3];//space for my option string
+    fgets(opt, sizeof(opt), stdin);//read input and store in the right variable
+
+    switch(opt){
+    case "1":
+      fputs("Enter the phrase you want to tokenize:\n>>", stdout);
+      fflush(stdout);
+
+      char str[100];//space for my string
+      fgets(str, sizeof(str), stdin);//read input
+
+      char **tokens =tokenize(str);
+      puts("These is your tokenized phrase: ");
+      print_tokens(tokens);
+      break;
+
+    case "2":
+      puts("1. Full history");
+      puts("2. 
+    }
+
+    
+  }
+  puts("What do you want to do?");
+  puts("1. Tokenize!");
+  puts("2. See history");
+  puts("3. Exit program");
+
+  fputs("Please enter one of the options (1-3): >>" stdout);
+  fflush(stdout);
   fputs("Enter the phrase you want to tokenize:\n>>", stdout);
   fflush(stdout);
 
@@ -14,6 +54,11 @@ int main(){
   printf("%s \n", str);
   char **tokens = tokenize(str);
   print_tokens(tokens);
+  List* history = init_history();
+  add_history(history, str);
+  char *get0 = get_history(history, 0);
+  printf("%s \n", get0);
+  print_history(history);
   
   return 0;
 }
